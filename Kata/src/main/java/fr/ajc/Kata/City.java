5c72edbd2e2709745f563e3037d4b77511e27b7f
Tanuelguy <manuel.tanguyr@protonmail.com>
2023-02-22 17:35:55 +0100

    première implémentation fonctionnelle

--- Kata/src/main/java/fr/ajc/Kata/City.java
+++ Kata/src/main/java/fr/ajc/Kata/City.java
@@ -1,5 +1,6 @@
 package fr.ajc.Kata;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -7,4 +8,9 @@ public class City {
 	private static List<String> cities = Arrays.asList("Paris", "Budapest", "Skopje", "Rotterdam", "Valencia", "Vancouver",
             "Amsterdam", "Vienna", "Sydney", "New York City", "London", "Bangkok", "Hong Kong", "Dubai", "Rome", "Istanbul");
 
+	
+	public static List<String> searchCities(String text){
+		List<String> result = new ArrayList<>();
+		return result;
+	}
 }
